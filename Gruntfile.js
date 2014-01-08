fdeaddc1b26bdab5a1603ee607ea99582fefa7b4
m7r <martin@figuro-theater.de>
2014-01-08 17:36:55 +0100

    feat(ngdocs): import unit tests from angularjs 1.2.7

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,24 @@
 module.exports = function (grunt) {
 
   grunt.loadNpmTasks('grunt-conventional-changelog');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-jasmine-node');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    changelog: { options: { dest: 'CHANGELOG.md' } }
+    changelog: { options: { dest: 'CHANGELOG.md' } },
+    jasmine_node: {
+      forceexit: true,
+      captureExceptions: true
+    },
+    watch: {
+      parser: {
+        files: ['src/*.js', 'spec/*Spec.js'],
+        tasks: ['jasmine_node']
+      }
+    }
   });
+
+  grunt.registerTask('test', 'Run tests for parser code', ['jasmine_node']);
 
 };
